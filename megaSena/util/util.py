ff5dd7362a1072f3e889112f780813a7e93dbf05
Thiago de Almeida Santos <103751658+thiagoalsa@users.noreply.github.com>
2022-05-05 19:33:40 +0100

    Create raffle function

--- megaSena/util/util.py
+++ megaSena/util/util.py
@@ -1,1 +1,9 @@
-print('Mega Sena')
+import random
+
+
+def raffle(number_of_numbers, game_size):
+    numbers_drawn = random.sample(range(1, game_size + 1), number_of_numbers)
+    return sorted(numbers_drawn)
+
+
+
